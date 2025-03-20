58c102405a5f0558951f4feab7a2bc4cc8d8af4c
Vanessa Arellano <vanessa.arellano@encora.com>
2025-03-19 22:19:39 -0600

    iterate through flight results

--- app/results/_components/list-flights.tsx
+++ app/results/_components/list-flights.tsx
@@ -1,11 +1,18 @@
 import { FlightComponent } from "./flight-component"
-import { demoFlight } from "../_consts/flight"
+import { demoFlights } from "../_consts/flight"
+import { Flight } from "./flight-component"
 
 export const FlightList = () => {
 
     return (
         <div className="p-10 h-full bg-white rounded-lg">
-            <FlightComponent flight={demoFlight} />
+            {
+                demoFlights.map((flight: Flight, index: number) => (
+                    <div key={index} className="mb-4">
+                        <FlightComponent flight={flight} />
+                    </div>
+                ))
+            }
         </div>
     )
 
--- app/results/_consts/flight.ts
+++ app/results/_consts/flight.ts
@@ -1,22 +1,44 @@
 import { Flight } from "../_components/flight-component";
 
-export const demoFlight: Flight = {
-    deperture_city: "San Francisco",
-    deperture_code: "(SFO)",
-    deperture_time: "7:30 AM", 
-    deperture_day: "10-02-2025", 
-    arraival_city: "Nueva York",
-    arraival_code: "(JFK)", 
-    arraival_time: "4:00 PM", 
-    arraival_day: "10-02-2025", 
-    airline: "Aeromexico",
-    total_price: "1400", 
-    currency: "USD", 
-    price_per_person: "700", 
-    number_stops: 1,
-    total_time: "8h 17m", 
-    stops: [{
-        stop_time: "1h 3m", 
-        stop_airport: "Los Angeles (LAX)"
-    }]
-}
+export const demoFlights: Flight[] = [
+    {
+        deperture_city: "San Francisco",
+        deperture_code: "(SFO)",
+        deperture_time: "7:30 AM", 
+        deperture_day: "10-02-2025", 
+        arraival_city: "Nueva York",
+        arraival_code: "(JFK)", 
+        arraival_time: "4:00 PM", 
+        arraival_day: "10-02-2025", 
+        airline: "Aeromexico",
+        total_price: "1400", 
+        currency: "USD", 
+        price_per_person: "700", 
+        number_stops: 1,
+        total_time: "8h 17m", 
+        stops: [{
+            stop_time: "1h 3m", 
+            stop_airport: "Los Angeles (LAX)"
+        }]  
+    }, 
+    {
+        deperture_city: "San Francisco",
+        deperture_code: "(SFO)",
+        deperture_time: "7:30 AM", 
+        deperture_day: "10-02-2025", 
+        arraival_city: "Nueva York",
+        arraival_code: "(JFK)", 
+        arraival_time: "4:00 PM", 
+        arraival_day: "10-02-2025", 
+        airline: "Aeromexico",
+        total_price: "1400", 
+        currency: "USD", 
+        price_per_person: "700", 
+        number_stops: 1,
+        total_time: "8h 17m", 
+        stops: [{
+            stop_time: "1h 3m", 
+            stop_airport: "Los Angeles (LAX)"
+        }]  
+    }, 
+]
